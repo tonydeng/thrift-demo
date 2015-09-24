e4d8b5f1061aec52767ad916b3d788fb8ab7a722
Tony Deng <wolf.deng@gmail.com>
2015-09-24 22:15:47 +0800

    添加PingPongService的SpringConfiguration

---
+++ src/main/java/com/github/tonydeng/demo/thrift/ping/server/SpringConfiguration.java
@@ -0,0 +1,36 @@
+package com.github.tonydeng.demo.thrift.ping.server;
+
+import com.github.tonydeng.demo.thrift.ping.api.PingPongService;
+import org.apache.thrift.TMultiplexedProcessor;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.server.TServlet;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.swing.text.StringContent;
+
+/**
+ * Created by tonydeng on 15/9/24.
+ */
+@Configuration
+@ComponentScan(basePackages = "com.github.tonydeng.demo.thrift.ping")
+public class SpringConfiguration {
+
+    @Bean
+    public TServlet thriftServlet(PingPongService.Iface pingPongService){
+        TMultiplexedProcessor multiplexedProcessor = new TMultiplexedProcessor();
+
+        PingPongService.Processor<PingPongService.Iface> pingPongProcessor = new PingPongService.Processor<>(pingPongService);
+
+        multiplexedProcessor.registerProcessor("PingPongService",pingPongProcessor);
+        TBinaryProtocol.Factory protocolFactory =  new TBinaryProtocol.Factory();
+        TServlet thriftServlet=new TServlet(multiplexedProcessor,protocolFactory);
+
+        LoggerFactory.getLogger(SpringConfiguration.class).info("Thrift sevlet initialized");
+
+        return thriftServlet;
+
+    }
+}
